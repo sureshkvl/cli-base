dd11c40456004e9b665349fa57c3fbf10c424e54
Suresh Kumar <sukumar.ext@orange.com>
2017-11-19 13:55:05 +0100

    rework continued

--- mCli/libs/shell.py
+++ mCli/libs/shell.py
@@ -2,12 +2,14 @@
 from __future__ import unicode_literals
 import os.path
 from six import text_type
-from pygments.token import Token
-
+#from pygments.token import Token
+from prompt_toolkit.token import Token
 from prompt_toolkit import prompt
 from prompt_toolkit.completion import Completer, Completion
 from prompt_toolkit.validation import Validator, ValidationError
 from prompt_toolkit.contrib.completers import WordCompleter
+from prompt_toolkit.history import InMemoryHistory
+from prompt_toolkit.auto_suggest import AutoSuggestFromHistory
 
 from mCli.utils import eventloop
 from mCli.libs.style import default as default_style
@@ -31,13 +33,20 @@ class Shell(object):
                 (Token.Symbol, self.symbol)
                 ]
 
-        my_completer = WordCompleter(self.CMgr.list("*"))
+        def get_bottom_toolbar_tokens(cli):
+            return [(Token.Toolbar, ' This is a toolbar. ')]
 
+
+        my_completer = WordCompleter(self.CMgr.list("*"))
+        history = InMemoryHistory()
         while True:
             try:
                 action = prompt(get_prompt_tokens=get_default_prompt_tokens,
+                                get_bottom_toolbar_tokens=get_bottom_toolbar_tokens,
                                 style=default_style,
                                 completer=my_completer,
+                                history=history,
+                                auto_suggest=AutoSuggestFromHistory(),
                                 eventloop=eventloop()
                                 )
             except (EOFError, KeyboardInterrupt):
